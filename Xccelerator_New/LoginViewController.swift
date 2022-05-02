d941c46665ca3436aa3b61fb50302f7b1c4103ca
kaylahharden <kaylahharden@gmail.com>
2022-05-02 13:04:07 -0400

    Added functionality of Sign up/ Login

--- Xccelerator_New/LoginViewController.swift
+++ Xccelerator_New/LoginViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginTapped(_ sender: Any) {
+        //Validate the TextFields
+        //Create cleaned version of the textfields
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        //Sign in the User
+        Auth.auth().signIn(withEmail: email, password: password){
+            (result, error) in
+            
+            if error != nil{
+                //Couldn't signin
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            }
+            else{
+                let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController)
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
     }
-    */
 
 }
